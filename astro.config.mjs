1b4ffa1ca046532446011cc25c542cf1d248662b
alexcurrimjee <alex.currimjee@gmail.com>
2026-04-05 20:59:18 +0100

    url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://alexcurrimjee.github.io',
-  base: '/brain-in-action-lab',
+  base: '/brain-in-action-lab/',
+  trailingSlash: 'always',
   output: 'static',
 });
